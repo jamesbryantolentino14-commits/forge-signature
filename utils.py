e5dc7530bee8cbf8048a2a072eeb39325907b8ec
James Bryan B. Tolentino <your-email@gmail.com>
2026-02-26 01:19:51 +1100

    Update signature validation, fix app design, ignore __pycache__

--- utils.py
+++ utils.py
@@ -11,9 +11,9 @@ def is_signature_like(image):
     edges = cv2.Canny(image, 50, 150)
     edge_density = np.sum(edges) / (image.shape[0] * image.shape[1])
 
-    if mean_intensity < 40:  # too dark
+    if mean_intensity < 20:  # too dark
         return False
-    if edge_density > 40:    # too complex (like a face)
+    if edge_density > 150:    # too complex (like a face)
         return False
 
     return True
